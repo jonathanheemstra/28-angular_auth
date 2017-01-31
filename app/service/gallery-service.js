c99c5dfcfba7454e50825d926f1c41f5d85da893
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-30 21:26:44 -0800

    set up gallery update and delete gallery routes

--- app/service/gallery-service.js
+++ app/service/gallery-service.js
@@ -60,7 +60,65 @@ function galleryService($q, $log, $http, authService) {
     });
   };
 
-  // TODO: add a delete galleries method
+  service.updateGallery = function(galleryID, galleryData) {
+    $log.debug('galleryService.updateGallery()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`;
+      let config = {
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      };
+      return $http.put(url, galleryData, config);
+    })
+    .then( res => {
+      for(let i = 0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if(current._id === galleryID) {
+          service.galleries[i] = res.data;
+          return;
+        }
+      }
+      return res.data;
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
+  service.deleteGallery = function(galleryID) {
+    $log.debug('galleryService.deleteGallery()');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryID}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      };
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      for(let i = 0; i < service.galleries.length; i++) {
+        let current = service.galleries[i];
+        if(current._id === galleryID) {
+          service.galleries.splice(i, 1);
+          return;
+        }
+      }
+      return res.data; // check this *****
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
 
   return service;
 }
