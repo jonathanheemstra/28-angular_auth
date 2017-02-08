7da95eea1d27c22b7943078397e58f363c652f4a
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-02-07 20:44:59 -0800

    add filter to require.context in entry.js

--- app/entry.js
+++ app/entry.js
@@ -45,3 +45,10 @@ context.keys().forEach( key => {
   let module = context(key);
   fomogram.directive(name, module);
 });
+
+context = require.context('./filter/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  fomogram.filter(name, module);
+});
